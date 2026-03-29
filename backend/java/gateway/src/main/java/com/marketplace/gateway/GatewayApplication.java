92d27e7e7b525eabe9e6c28963ca62d3a1c42f74
Adam Algis <pootivod@gmail.com>
2026-03-29 14:47:26 +0300

    new

--- backend/java/gateway/src/main/java/com/marketplace/gateway/GatewayApplication.java
+++ backend/java/gateway/src/main/java/com/marketplace/gateway/GatewayApplication.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<<< HEAD:backend/java/gateway/src/main/java/com/marketplace/gateway/GatewayApplication.java
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-========
-public class GatewayApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApiApplication.class, args);
->>>>>>>> adam:backend/java/gateway-api/src/main/java/com/marketplace/gateway/GatewayApiApplication.java
     }
 }
